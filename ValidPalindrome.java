9941869f690c59e952fbe97b6d9c921ec9f5fb96
Nayan.Dahare <nayandahare02@gmail.com>
2026-02-18 21:53:33 +0530

    Valid Palindrome

---
+++ ValidPalindrome.java
@@ -0,0 +1,26 @@
+public class ValidPalindrome {
+    public static void main(String[] args) {
+        System.out.println("Execution Strats :");
+        String s = "Nayana";
+        int start = 0;
+        int end = s.length() - 1;
+
+        while (start < end) {
+            while (start < end && !Character.isLetterOrDigit(s.charAt(start))) {
+                start++;
+            }
+
+            while (start < end && !Character.isLetterOrDigit(s.charAt(end))) {
+                end--;
+            }
+            // char startChar = s.charAt(start);
+            // char endChar = s.charAt(end);
+            if (Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) {
+                System.out.println("Its not palindrome");
+            }
+            start++;
+            end--;
+        }
+        System.out.println("Its palindrome");
+    }
+}
